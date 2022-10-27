da89228a1ea79e6e2fead4e6c27aa84c9f270578
EugGolovanov <86769060+EugGolovanov@users.noreply.github.com>
2022-10-27 20:45:56 +0300

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import catboost as cb
-from preprocess import params
-from preprocess import get_treshholds
-from preprocess import make_full_pred
+from preprocess import *
 from sklearn.metrics import recall_score
 data_dir = './data'
 X_train = pd.read_csv(data_dir + '/X_train.csv')
@@ -32,6 +30,63 @@ clf_sub.fit(submit_pool_train)
 pred_proba = clf_sub.predict_proba(submit_pool_test)
 real_test = pd.read_csv('/content/test.csv')
 min_t1, min_t2, min_error = get_tresholds(pred_proba, real_test)
+pred_job = [0 if i[0] < min_t1 else 1 for i in pred_proba]
+pred_div = [0 if i[1] < min_t2 else 1 for i in pred_proba]
+pred = make_full_pred(pred_job, pred_div)
+real_test['type'] = pred
+df_full = pd.read_csv(data_dir + '/full_data.csv')
+df_full['id'] = df_full.index
+df_full['type'] = 3
+df_full['job'] = 0
+df_full['division'] = 0
+df_train = pd.read_csv(data_dir + "/train.csv")
+df_test = pd.read_csv(data_dir + "/test.csv")
+df_train = pd.merge(df_train, df_full, on='id', how='left')
+df_test = pd.merge(df_test, df_full, on='id', how='left')
+df_train = df_train.fillna(0)
+df_test = df_test.fillna(0)
+lst_drop = get_lst_drop(df_train.columns)
+lst_test_drop = get_lst_drop(df_test.columns, is_test=True)
+cats = get_cats(df_train.columns)
+df_train[cat] = df_train[cat].astype('string') 
+df_test[cat] = df_test[cat].astype('string') 
+aux_tmp = df_full.drop(lst_drop, axis=1)
+aux_tmp = aux_tmp.fillna(0)
+aux_tmp[cat] = aux_tmp[cat].astype('string')
+X_auxiliary = cb.Pool(aux_tmp, cat_features=cat)
+id_auxiliary = df_full.index
+pred_proba_auxiliary = clf_sub.predict_proba(X_auxiliary)
+# N = 400 # топ 400 минимальных по вероятности 3его класса 
+tmp = pd.DataFrame()
+f = lambda x, k: [i[k] for i in x]
+tmp['proba_1'] = f(pred_proba_auxiliary, 0) 
+tmp['proba_2'] = f(pred_proba_auxiliary, 1) 
+tmp['proba_1'] = tmp['proba_1'] < min_t1-0.1
+tmp['proba_2'] = tmp['proba_2'] < min_t2-0.1
+idxs = tmp.loc[tmp['proba_1'] & tmp['proba_2']].index
+auxiliary = df_full.iloc[idxs]
+auxiliary['type'] = 3
+auxiliary['job'] = 0
+auxiliary['division'] = 0
+X_train = pd.concat([df_train, auxiliary], axis=0)
+X_train['division'] = X_train['type'].apply(change_division)
+X_train['job'] = X_train['type'].apply(change_job)
+y_train = X_train[['job', 'division']].fillna(0)
+aux_tmp = X_train.drop(lst_drop, axis=1)
+aux_tmp = aux_tmp.fillna(0)
+aux_tmp[cat] = aux_tmp[cat].astype('string')
+pool_train = cb.Pool(aux_tmp, y_train, cat_features=cat)
+params = {                  # 'task_type':'GPU',
+                            'iterations':1200,
+                            'depth':7,
+                            'random_state':SEED,
+                            'learning_rate':0.01,
+                            'eval_metric':'HammingLoss',
+                            'loss_function':'MultiCrossEntropy'}
+clf_with_aux = cb.CatBoostClassifier(**params)
+clf_with_aux.fit(pool_train)
+pred_proba = clf_with_aux.predict_proba(submit_pool_test)
+min_t1, min_t2, min_error = get_tresholds(pred_proba)
 pred_job = [0 if i[0] < min_t1 else 1 for i in pred_proba]
 pred_div = [0 if i[1] < min_t2 else 1 for i in pred_proba]
 pred = make_full_pred(pred_job, pred_div)
